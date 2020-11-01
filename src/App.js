28594dcc08db49669c60f617a06e073ada81d663
zifstark <nazifbarassounon@gmail.com>
2020-11-01 14:35:30 +0100

    Add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { FiShoppingCart } from 'react-icons/fi';
+
 function App() {
   return (
-    <p>
-      Hello
-    </p>
+    <div>
+      <header >
+        <div className="Container">
+          <div className="HeaderBar">
+            <span className="AppName">OnlineGameStore</span>
+            <span className="Cart"><FiShoppingCart/></span>
+          </div>
+        </div>
+      </header>
+    </div>
   );
 }
 
