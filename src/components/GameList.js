191feafa25c5f65375172b02a4f9e68fae4e1afc
zifstark <nazifbarassounon@gmail.com>
2020-11-03 12:24:29 +0100

    Add key to each child of GameList

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -6,7 +6,7 @@ import Game from './Game';
 function GameList({ list }) {
   return (
     <section className="GameList">
-      {list.map(g => <Game item={g} />)}
+      {list.map(g => <Game key={g.id} item={g} />)}
     </section>
   );
 };
