ccbe3448bb71994da66db99363eb2ae5eb4b5ff8
zifstark <nazifbarassounon@gmail.com>
2020-11-03 12:21:07 +0100

    Put games in a state

--- src/App.js
+++ src/App.js
@@ -1,58 +1,70 @@
+import { useReducer } from 'react';
 import { FiShoppingCart } from 'react-icons/fi';
 import GameList from './components/GameList';
 
-const games = [
-  {
-    id: "111",
-    title: "The witcher 3",
-    imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
-    price: "49.99",
-  },
-  {
-    id: "222",
-    title: "The witcher 3",
-    imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
-    price: "49.99",
-  },
-  {
-    id: "333",
-    title: "The witcher 3",
-    imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
-    price: "49.99",
-  },
-  {
-    id: "444",
-    title: "The witcher 3",
-    imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
-    price: "49.99",
-  },
-  {
-    id: "555",
-    title: "The witcher 3",
-    imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
-    price: "49.99",
-  },
-  {
-    id: "666",
-    title: "The witcher 3",
-    imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
-    price: "49.99",
-  },
-  {
-    id: "777",
-    title: "The witcher 3",
-    imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
-    price: "49.99",
-  },
-  {
-    id: "888",
-    title: "The witcher 3",
-    imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
-    price: "49.99",
-  },
-]
+const initialGamesState = {
+  data: [
+    {
+      id: "111",
+      title: "The witcher 3",
+      imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
+      price: "49.99",
+    },
+    {
+      id: "222",
+      title: "The witcher 3",
+      imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
+      price: "49.99",
+    },
+    {
+      id: "333",
+      title: "The witcher 3",
+      imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
+      price: "49.99",
+    },
+    {
+      id: "444",
+      title: "The witcher 3",
+      imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
+      price: "49.99",
+    },
+    {
+      id: "555",
+      title: "The witcher 3",
+      imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
+      price: "49.99",
+    },
+    {
+      id: "666",
+      title: "The witcher 3",
+      imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
+      price: "49.99",
+    },
+    {
+      id: "777",
+      title: "The witcher 3",
+      imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
+      price: "49.99",
+    },
+    {
+      id: "888",
+      title: "The witcher 3",
+      imageUrl: "https://planete-play.fr/images/2014/06/The-Witcher-3-Wild-Hunt-Jaquette-PS4.jpg",
+      price: "49.99",
+    },
+  ]
+}
+
+function gamesReducer(state, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [games, dispatchGames] = useReducer(gamesReducer, initialGamesState);
+
   return (
     <div>
       <header>
@@ -66,7 +78,7 @@ function App() {
 
       <h2 className="Title">Play Has No Limits</h2>
 
-      <GameList list={games}/>
+      <GameList list={games.data}/>
     </div>
   );
 }
