708e7ab1fad82528aad2312798e3254537976461
zifstark <nazifbarassounon@gmail.com>
2020-11-03 11:55:50 +0100

    Add 'add to cart' button

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './Game.css';
 import { FiDollarSign } from 'react-icons/fi';
 
+
 function Game({ item }) {
   return (
     <article className="Game" key={item.id}>
       <img alt="" src={item.imageUrl} />
-      <h3>{item.title}</h3>
-      <span><FiDollarSign/>{item.price}</span>
+      <div className="GameMeta">
+        <div>
+          <h3>{item.title}</h3>
+          <span><FiDollarSign/>{item.price}</span>
+        </div>
+        <button>Add to cart</button>
+      </div>
     </article>
   )
 }
