3109a79bd8edc3c39071dccefe0d85ac0d4f660e
Nazif Barassounon <nazifbarassounon@gmail.com>
2021-06-20 16:28:45 +0100

    test(AddToCart): ensure the button can remove the item from cart

--- src/components/add_to_cart/add_to_cart.test.jsx
+++ src/components/add_to_cart/add_to_cart.test.jsx
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import AddToCart from './add_to_cart';
 import { CartProvider, useCart } from '../../context/cart_context';
+
+afterEach(() => {
+  const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
+  const { result } = renderHook(() => useCart(), { wrapper });
+  act(() => {
+    result.current.clearCart();
+  });
+});
 
 test('can add item to cart', () => {
   const item = {
@@ -24,7 +32,7 @@ test('can add item to cart', () => {
 
   expect(button).toHaveTextContent(/remove from cart/i);
 
-  const { result } = renderHook(() => useCart(), { wrapper });
+  const { result } = renderHook(useCart, { wrapper });
 
   expect(result.current.cartItems).toHaveLength(1);
   expect(result.current.cartItems[0]).toEqual({
@@ -33,4 +41,41 @@ test('can add item to cart', () => {
     price: 1000,
     quantity: 1,
   });
+});
+
+test('can remove item from cart', async () => {
+  const item = {
+    id: 'theid',
+    imageUrl: '',
+    title: 'GTA',
+    price: 1000,
+  };
+
+  const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
+
+  render(<AddToCart item={item} />, { wrapper });
+
+  const button = screen.getByRole('button', { name: /add to cart/i });
+
+  expect(button).toBeInTheDocument();
+
+  userEvent.click(button);
+
+  expect(button).toHaveTextContent(/remove from cart/i);
+
+  let rh = renderHook(useCart, { wrapper });
+
+  expect(rh.result.current.cartItems).toHaveLength(1);
+  expect(rh.result.current.cartItems[0]).toEqual({
+    id: 'theid',
+    title: 'GTA',
+    price: 1000,
+    quantity: 1,
+  });
+
+  userEvent.click(button);
+
+  rh = renderHook(useCart, { wrapper });
+
+  expect(rh.result.current.cartItems).toHaveLength(0);
 });
