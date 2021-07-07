fa475b502f66d4be91418105fa06c3e40f7e9a73
Nazif Barassounon <nazifbarassounon@gmail.com>
2021-07-07 16:49:43 +0100

    update cart page ui

--- src/routes/cart_page/cart_page.jsx
+++ src/routes/cart_page/cart_page.jsx
@@ -1,5 +1,7 @@
 import './cart_page.css';
 import { Link } from 'react-router-dom';
+import PlusIcon from './plus_icon';
+import MinusIcon from './minus_icon';
 import Button from '../../components/button';
 import { useCart } from '../../context/cart_context';
 import { printPrice } from '../../utils';
@@ -16,7 +18,12 @@ function CartPage() {
   if (getCartTotal() === 0) {
     return (
       <div className="Container">
-        <h2 style={{ color: 'red', marginTop: '15px' }}>Your cart is empty</h2>
+        <h2>Your cart is currently empty.</h2>
+        <div style={{ width: '200px', marginTop: '10px' }}>
+          <Button type="primary">
+            <Link to="/">Return To Shop</Link>
+          </Button>
+        </div>
       </div>
     );
   }
@@ -35,27 +42,39 @@ function CartPage() {
         <tbody>
           {cartItems.map((i) => (
             <tr key={i.id}>
-              <td>{i.title}</td>
+              <td>
+                <span>{i.title}</span>
+                <button onClick={() => onItemRemove(i)} className="remove-btn">
+                  remove
+                </button>
+              </td>
               <td>{printPrice(i.price)}</td>
               <td>
-                <input
-                  type="number"
-                  min="1"
-                  value={i.quantity}
-                  onChange={(e) => {
-                    onQuantityChange(i, e.target.value);
-                  }}
-                />
-                <br />
-                <button onClick={() => onItemRemove(i)}>remove</button>
+                <div className="qty-picker">
+                  <button
+                    onClick={() => onQuantityChange(i, i.quantity + 1)}
+                    className="qty-btn"
+                  >
+                    <PlusIcon />
+                  </button>
+                  <span>{i.quantity}</span>
+                  <button
+                    onClick={() => onQuantityChange(i, i.quantity - 1)}
+                    className="qty-btn"
+                  >
+                    <MinusIcon />
+                  </button>
+                </div>
               </td>
               <td>{printPrice(getItemTotal(i))}</td>
             </tr>
           ))}
         </tbody>
       </table>
-      <div style={{ marginBottom: '15px' }}>
-        <strong>Total: {printPrice(getCartTotal())}</strong>
+      <div style={{ margin: '15px 0', fontSize: '20px' }}>
+        <p>
+          <strong>Total: {printPrice(getCartTotal())}</strong>
+        </p>
       </div>
       <Button type="primary">
         <Link to="/checkout">Procced to checkout</Link>
